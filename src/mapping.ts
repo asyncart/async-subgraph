8da8293c271024fb476e678784379f4053ffa47c
Kinrezc <matt.czernik@gmail.com>
2020-06-02 21:30:53 -0400

    save correct current lever state, handle accounts properly, differentiate between sale, transfer and bid log

--- src/mapping.ts
+++ src/mapping.ts
@@ -24,36 +24,109 @@ import {
   Layer,
   Lever,
 } from "../generated/schema";
-import { loadOrCreateAccount, loadOrCreatePlatform } from "./factory";
-
-const GENESIS_ADDRESS = "0x0000000000000000000000000000000000000000";
+import { loadOrCreatePlatform } from "./factory";
 
 export function handleApproval(event: Approval): void {}
 
 export function handleApprovalForAll(event: ApprovalForAll): void {}
 
 export function handleBidProposed(event: BidProposed): void {
-  // TODO: Derive all bids from accounts and link to master/layer tokenId
   let tokenId = event.params.tokenId.toString();
   let master = Master.load(tokenId);
 
-  let id = event.transaction.hash.toHexString();
-  let bid = new BidLog(id);
+  let account = Account.load(event.params.bidder.toHexString());
+
+  let i = 0;
+  let bidId = "";
+  let incrementBidId = true;
+  while (incrementBidId == true) {
+    let existingBidFromBidder = BidLog.load(
+      event.params.bidder.toHexString() +
+        "-" +
+        event.params.tokenId.toString() +
+        "-" +
+        i.toString()
+    );
+    if (existingBidFromBidder == null) {
+      bidId =
+        event.params.bidder.toHexString() +
+        "-" +
+        event.params.tokenId.toString() +
+        "-" +
+        i.toString();
+      incrementBidId = false;
+    } else {
+      i++;
+    }
+  }
+
+  let bid = new BidLog(bidId);
   bid.timestamp = event.block.timestamp;
-  bid.amountInWei = event.params.bidAmount;
   bid.isWithdrawn = false;
-  bid.bidder = event.params.bidder;
+  bid.wasSale = false;
+  bid.amountInWei = event.params.bidAmount;
+
+  if (account == null) {
+    account = new Account(event.params.bidder.toHexString());
+    account.isArtist = false;
+    account.save();
+  }
+
+  bid.bidder = event.params.bidder.toHexString();
 
   if (master !== null) {
     bid.master = tokenId;
     bid.save();
+
+    master.pastBids.push(bidId);
+    if (master.highBid == null) {
+      master.highBid = bidId;
+    } else {
+      let currentHighBidLog = BidLog.load(master.highBid);
+      if (currentHighBidLog.amountInWei < bid.amountInWei) {
+        master.highBid = bidId;
+      }
+    }
+    master.save();
   } else {
     bid.layer = tokenId;
     bid.save();
+
+    let layer = Layer.load(tokenId);
+    layer.pastBids.push(bidId);
+
+    if (layer.highBid == null) {
+      layer.highBid = bidId;
+    } else {
+      let currentHighBidLog = BidLog.load(layer.highBid);
+      if (currentHighBidLog.amountInWei < bid.amountInWei) {
+        layer.highBid = bidId;
+      }
+    }
+    layer.save();
   }
 }
 
-export function handleBidWithdrawn(event: BidWithdrawn): void {}
+export function handleBidWithdrawn(event: BidWithdrawn): void {
+  let i = 0;
+  let incrementBidId = true;
+  while (incrementBidId == true) {
+    let existingBidFromBidder = BidLog.load(
+      event.transaction.from.toHexString() +
+        "-" +
+        event.params.tokenId.toString() +
+        "-" +
+        i.toString()
+    );
+    if (existingBidFromBidder.isWithdrawn == false) {
+      existingBidFromBidder.isWithdrawn = true;
+      existingBidFromBidder.save();
+      incrementBidId = false;
+    } else {
+      i++;
+    }
+  }
+}
 
 export function handleBuyPriceSet(event: BuyPriceSet): void {
   let id = event.params.tokenId.toString();
@@ -70,7 +143,37 @@ export function handleBuyPriceSet(event: BuyPriceSet): void {
 }
 
 export function handleControlLeverUpdated(event: ControlLeverUpdated): void {
-  // TODO implement layer control mapping if needed
+  let tokenId = event.params.tokenId.toString();
+  let layer = Layer.load(tokenId);
+  let master = Master.load(layer.master);
+
+  let numUpdatedIds = event.params.leverIds.length;
+  let i = 0;
+  while (i < numUpdatedIds) {
+    let lever = Lever.load(
+      "layer-" + tokenId + "-" + event.params.leverIds.pop().toString()
+    );
+    if (lever !== null) {
+      lever.currentValue = event.params.updatedValues.pop().toI32();
+      lever.save();
+    } else {
+      log.log(log.Level.DEBUG, "Cannot update null lever id");
+    }
+    i++;
+  }
+  let updateId =
+    event.transaction.hash.toHexString() +
+    "-" +
+    event.block.timestamp.toString();
+
+  layer.lastUpdate = updateId;
+  master.lastUpdate = updateId;
+
+  layer.numUpdates = layer.numUpdates + 1;
+  master.numUpdates = master.numUpdates + 1;
+
+  master.save();
+  layer.save();
 }
 
 export function handlePlatformAddressUpdated(
@@ -95,6 +198,15 @@ export function handleMintArtwork(call: MintArtworkCall): void {
   let masterId = call.inputs.artworkTokenId.toString();
   let master = new Master(masterId);
 
+  let account = Account.load(call.from.toHexString());
+
+  if (account == null) {
+    account = new Account(call.from.toHexString());
+  }
+
+  account.isArtist = true;
+  account.save();
+
   let layerCount = call.inputs.controlTokenArtists.length;
 
   let startId = call.inputs.artworkTokenId;
@@ -105,19 +217,21 @@ export function handleMintArtwork(call: MintArtworkCall): void {
     let layerId = startId.plus(incrementor).toString();
     let layer = new Layer(layerId);
 
-    layer.owner = call.from;
-    layer.creators = [call.from];
-    layer.pastOwners = [call.from];
+    layer.owner = call.from.toHexString();
+    layer.creators = [call.from.toHexString()];
+    layer.pastOwners = [call.from.toHexString()];
     layer.master = masterId;
+    layer.numUpdates = 0;
     layer.save();
     incrementor = incrementor.plus(BigInt.fromI32(1));
     i++;
   }
   master.uri = call.inputs.artworkTokenURI.toString();
-  master.creators = [call.from];
+  master.creators = [call.from.toHexString()];
   master.uri = call.inputs.artworkTokenURI;
-  master.owner = call.from;
-  master.pastOwners = [call.from];
+  master.owner = call.from.toHexString();
+  master.numUpdates = 0;
+  master.pastOwners = [call.from.toHexString()];
   master.layerCount = layerCount;
   master.save();
 }
@@ -149,18 +263,29 @@ export function handleTokenSale(event: TokenSale): void {
   let tokenId = event.params.tokenId.toString();
   let master = Master.load(tokenId);
 
-  let transferLog = new TransferLog(event.transaction.hash.toHexString());
-  transferLog.timestamp = event.block.timestamp;
-  transferLog.from = event.transaction.from as Bytes;
-  transferLog.to = event.transaction.to as Bytes;
-  transferLog.tokenId = tokenId.toString();
+  let saleLog = new SaleLog(event.transaction.hash.toHexString());
+  saleLog.timestamp = event.block.timestamp;
+  saleLog.seller = event.transaction.from.toHexString();
+  saleLog.buyer = event.transaction.to.toHexString();
+  saleLog.tokenId = tokenId.toString();
 
   if (master !== null) {
-    transferLog.master = tokenId;
-    transferLog.save();
+    let acceptedBid = BidLog.load(master.highBid);
+    saleLog.amountInWei = acceptedBid.amountInWei;
+    saleLog.master = tokenId;
+    saleLog.save();
+    master.highBid = null;
+    master.lastSale = event.transaction.hash.toHexString();
+    master.save();
   } else {
-    transferLog.layer = tokenId;
-    transferLog.save();
+    let layer = Layer.load(tokenId);
+    let acceptedBid = BidLog.load(layer.highBid);
+    saleLog.amountInWei = acceptedBid.amountInWei;
+    saleLog.layer = tokenId;
+    saleLog.save();
+    layer.highBid = null;
+    layer.lastSale = event.transaction.hash.toHexString();
+    layer.save();
   }
 }
 
@@ -168,10 +293,21 @@ export function handleTransfer(event: Transfer): void {
   let tokenId = event.params.tokenId.toString();
   let master = Master.load(tokenId);
 
+  let toAccount = Account.load(event.params.to.toHexString());
+  let fromAccount = Account.load(event.params.from.toHexString());
+
+  if (toAccount == null) {
+    toAccount = new Account(event.params.to.toHexString());
+  }
+
+  if (fromAccount == null) {
+    fromAccount = new Account(event.params.from.toHexString());
+  }
+
   let transferLog = new TransferLog(event.transaction.hash.toHexString());
   transferLog.timestamp = event.block.timestamp;
-  transferLog.from = event.transaction.from as Bytes;
-  transferLog.to = event.transaction.to as Bytes;
+  transferLog.from = event.transaction.from.toHexString();
+  transferLog.to = event.transaction.to.toHexString();
   transferLog.tokenId = tokenId.toString();
 
   if (master !== null) {
